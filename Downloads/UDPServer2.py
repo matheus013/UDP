349ea2c8c6086e256bbc3976cb62430ad2f33b81
Pkzz <nombregag@gmail.com>
2016-08-25 21:11:40 -0300

    Inicial Commit

--- Downloads/UDPServer2.py
+++ Downloads/UDPServer2.py
@@ -8,21 +8,37 @@ sock = socket.socket(socket.AF_INET,  # Internet
 sock.bind((UDP_IP, UDP_PORT))
 
 log = []
+clients = []
 
-print "\n[+] New server socket created\n"
-
-f = open('/home/glins/Downloads/serverlog.txt', 'w')
+print "[+] New server socket created\n"
+print " - Server Port:"+ str(UDP_PORT)
 
 while True:
     data, (ip, port) = sock.recvfrom(1024)  # buffer size is 1024 bytes
-    if data == "SAVELOG":
-        for item in log:
-            f.write("\n".join(map(lambda x: str(x), log)) + "\n")
-        f.close()
 
-    print "Received addr:", (ip, port)
-    log.append("["+datetime.datetime.now().time().__str__()+"] Message:" + data + " IP:"+ip)
+    if (ip, port) not in clients:
+        clients.append((ip, port))
+
+    if data.find("SAVELOG")==0:
+        with open("serverlogs.txt", "w")as fp:
+            for line in log:
+                fp.write(line + "\n")
+        fp.close()
+        sock.sendto("Saved server logs successfully.", (ip, port))
+        continue
+
+    if data.find("CLEARLOG")==0:
+        del log[:]
+        sock.sendto("Cleared server logs successfully.", (ip, port))
+        continue
+
+    if data.find("WHOAMI")==0:
+        sock.sendto("Client " + (clients.index((ip, port))+1).__str__(), (ip, port))
+        continue
+
+    print "[" + ip + "] Sent: " + data
+    log.append("["+datetime.datetime.now().time().__str__()+"] Message:" + data + "\n - [IP:"+ip + "] Client " + (clients.index((ip, port))+1).__str__())
 
     #  modifiedMessage = data.upper()
-    sock.sendto("seus lixos absolutos", (ip, port))
+    sock.sendto("Received \"" + data + "\"", (ip, port))
     #  print "Sent message:", modifiedMessage
